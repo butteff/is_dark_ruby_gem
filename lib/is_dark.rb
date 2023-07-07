8d611ed8dde06a09db891aa46eee141831b41d14
butteff <illarionov@protonmail.com>
2023-07-07 23:16:37 +0300

    editable percentage, dots range matrix values + debug mode output + draw area in a file to test the valid area (v 0.0.6)

--- lib/is_dark.rb
+++ lib/is_dark.rb
@@ -4,6 +4,9 @@ class IsDark
 	@g = 0
 	@b = 0
 	@colorset = 255
+	@with_debug = false
+	@with_debug_file = false
+	@debug_file_path = '/tmp/is_dark_debug_file.pdf'
 
 	def self.color(hex)
 		@r, @g, @b = hex.match(/^#(..)(..)(..)$/).captures.map(&:hex)
@@ -11,49 +14,94 @@ class IsDark
 		self.is_dark
 	end
 
-	def self.magick_pixel(pix)
-      	@r, @g, @b = [pix.red.to_f, pix.green.to_f, pix.blue.to_f]
+	def self.magick_pixel(pix, x=nil, y=nil)
+    @r, @g, @b = [pix.red.to_f, pix.green.to_f, pix.blue.to_f]
 		@colorset = 655*255
-		self.is_dark
+		is = self.is_dark(x, y)
+    is
 	end
 
 	def self.magick_pixel_from_blob(x, y, blob)
 		image = Magick::Image.read(blob).first
 		pix = image.pixel_color(x, y)
-      	self.magick_pixel(pix)
+    self.magick_pixel(pix, x, y)
 	end
+																	 
+  def self.magick_area_from_blob(x, y, blob, height, width, percent=80, range=(0..10)) # (x, y) is the left corner of an element over a blob, height and width is the element's size
+    image = Magick::Image.read(blob).first
+    dark = false
+    dots = []
+    range.each { |xx|
+      range.each { |yy|
+        dots << {'x': (x+width*xx/10).to_i, 'y': (y+height*yy/10).to_i}
+      }
+    }
 
-	def self.magick_area_from_blob(x, y, blob, height, width) # (x, y) is the left corner of an element over a blob, height and width is the element's size
-	    image = Magick::Image.read(blob).first
-	    a = x+width
-	    b = y-height
-	    dots = [{'x': x, 'y': y}, {'x': a, 'y': y}, {'x': a, 'y': b}, {'x': x, 'y': b}, {'x': a/2, 'y': b/2}, {'x': a/4, 'y': b/2}, {'x': a/2, 'y': b/4}, {'x': a/2, 'y': (b/4)*3}, {'x': (a/4)*3, 'y': b/2}, {'x': a/3, 'y': y}, {'x': (a/3)*2, 'y': y}, {'x': a/3, 'y': b}, {'x': (a/3)*2, 'y': b}, {'x': x, 'y': b/3}, {'x': x, 'y': (b/3)*2}, {'x': a, 'y': b/3}, {'x': a, 'y': (b/3)*2}, {'x': a/3, 'y': b/3}, {'x': a/3, 'y': (b/3)*2}, {'x': (a/3)*2, 'y': b/3}, {'x': (a/3)*2, 'y': (b/3)*2}]
-	    points = 0
-	    dots.each { |dot|
-	      x = dot[:x]
-	      y = dot[:y]
-	      pix = image.pixel_color(x, y)
-	      l = self.magick_pixel(pix)
-	      points += 1 if l
-	    }
-	    dark = true if points >= 16
-	    dark
-  	end
+    points = 0
+    if @with_debug_file
+    	oldx = false
+    	oldy = false
+    end
+    p '====================================================================' if @with_debug
+    dots.each { |dot|
+      x = dot[:x].to_i
+      y = dot[:y].to_i
+      pix = image.pixel_color(x, y)
+      l = self.magick_pixel(pix, x, y)
+      points += 1 if l
+      if @with_debug_file
+      	self.draw_debug_files(image, x, y, oldx, oldy) 
+      	oldy = y
+      	oldx = x
+      end
+    }
+    dark = true if points >= (dots.length/100)*percent
+    if @with_debug
+    	p '===================================================================='
+    	p 'Total Points: '+dots.length.to_s+', dark points amount:'+points.to_s
+    	p 'Signature will be inverted if '+percent.to_s+'% of dots will be dark'
+    	p 'Is inverted: '+dark.to_s
+    	p '===================================================================='
+    end
+    dark
+  end
+
+  def self.set_debug_data(show_info=false, draw_file=false)
+    @with_debug=show_info
+    if draw_file != false
+    	@with_debug_file = true
+    	@debug_file_path = draw_file
+    end
+  end
 
 	private
 
-	def self.is_dark #detects a dark color based on luminance W3 standarts ( https://www.w3.org/TR/WCAG20/#relativeluminancedef )
+	def self.draw_debug_files(image, x, y, oldx, oldy)
+		if oldx && oldy
+			gc = Magick::Draw.new
+			gc.line(x, y, oldx, oldy)
+			gc.stroke('black')
+			gc.draw(image)
+			image.write(@debug_file_path)
+		end
+	end
+
+	def self.is_dark(x=nil, y=nil) #detects a dark color based on luminance W3 standarts ( https://www.w3.org/TR/WCAG20/#relativeluminancedef )
 		dark = false
 		pixel = [@r.to_f, @g.to_f, @b.to_f]
 		calculated = []
 		pixel.each { |color|
-		    color = color/@colorset
-		    color = color/12.92 if color <= 0.03928
-		    color = ((color+0.055)/1.055)**2.4 if color > 0.03928
-		    calculated << color
+		  color = color/@colorset
+		  color = color/12.92 if color <= 0.03928
+		  color = ((color+0.055)/1.055)**2.4 if color > 0.03928
+		  calculated << color
 		}
 		l = (0.2126*calculated[0] + 0.7152*calculated[1] + 0.0722*calculated[2])
 		dark = true if l <= 0.05
+		if @with_debug
+			debug = {'X': x, 'Y': y, 'R': @r, 'G': @g, 'B': @b, 'luminance value': l, 'is_dark': dark}
+			p debug
+		end
 		dark
 	end
 end
