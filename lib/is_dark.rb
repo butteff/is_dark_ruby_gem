fab5c1792c1c667463d0de7472b084efa83044ca
butteff <illarionov@protonmail.com>
2023-07-08 16:18:30 +0300

    gem 0.0.7 version with an option to mark not detected pixels by imagick as white

--- lib/is_dark.rb
+++ lib/is_dark.rb
@@ -14,20 +14,21 @@ class IsDark
 		self.is_dark
 	end
 
-	def self.magick_pixel(pix, x=nil, y=nil)
+	def self.magick_pixel(pix, x=nil, y=nil, set_not_detected_light=true)
     @r, @g, @b = [pix.red.to_f, pix.green.to_f, pix.blue.to_f]
 		@colorset = 655*255
-		is = self.is_dark(x, y)
+		is = self.is_dark(x, y, set_not_detected_light)
     is
 	end
 
-	def self.magick_pixel_from_blob(x, y, blob)
+	def self.magick_pixel_from_blob(x, y, blob, set_not_detected_light=true)
 		image = Magick::Image.read(blob).first
 		pix = image.pixel_color(x, y)
     self.magick_pixel(pix, x, y)
 	end
 																	 
-  def self.magick_area_from_blob(x, y, blob, height, width, percent=80, range=(0..10)) # (x, y) is the left corner of an element over a blob, height and width is the element's size
+  def self.magick_area_from_blob(x, y, blob, height, width, percent=80, range=(0..10), set_not_detected_light=true) # (x, y) is the left corner of an element over a blob, height and width is the element's size
+    @set_not_detected_light = set_not_detected_light
     image = Magick::Image.read(blob).first
     dark = false
     dots = []
@@ -47,7 +48,7 @@ class IsDark
       x = dot[:x].to_i
       y = dot[:y].to_i
       pix = image.pixel_color(x, y)
-      l = self.magick_pixel(pix, x, y)
+      l = self.magick_pixel(pix, x, y, set_not_detected_light)
       points += 1 if l
       if @with_debug_file
       	self.draw_debug_files(image, x, y, oldx, oldy) 
@@ -59,8 +60,9 @@ class IsDark
     if @with_debug
     	p '===================================================================='
     	p 'Total Points: '+dots.length.to_s+', dark points amount:'+points.to_s
+    	p 'Is "invert to white not detectd pixels" option enabled?:'+set_not_detected_light.to_s
     	p 'Signature will be inverted if '+percent.to_s+'% of dots will be dark'
-    	p 'Is inverted: '+dark.to_s
+    	p 'Is inverted?: '+dark.to_s
     	p '===================================================================='
     end
     dark
@@ -86,9 +88,14 @@ class IsDark
 		end
 	end
 
-	def self.is_dark(x=nil, y=nil) #detects a dark color based on luminance W3 standarts ( https://www.w3.org/TR/WCAG20/#relativeluminancedef )
+	def self.is_dark(x=nil, y=nil, set_not_detected_light=true) #detects a dark color based on luminance W3 standarts ( https://www.w3.org/TR/WCAG20/#relativeluminancedef )
 		dark = false
+		inverted = false
 		pixel = [@r.to_f, @g.to_f, @b.to_f]
+		if set_not_detected_light && pixel[0] == 0.0 && pixel[1] == 0.0 && pixel[2] == 0.0 #probably not detected pixel color by Imagick, will be considered as "white" if "set_not_detected_light = true"
+			pixel = [65535.0, 65535.0, 65535.0]
+			inverted = true 
+		end
 		calculated = []
 		pixel.each { |color|
 		  color = color/@colorset
@@ -99,7 +106,7 @@ class IsDark
 		l = (0.2126*calculated[0] + 0.7152*calculated[1] + 0.0722*calculated[2])
 		dark = true if l <= 0.05
 		if @with_debug
-			debug = {'X': x, 'Y': y, 'R': @r, 'G': @g, 'B': @b, 'luminance value': l, 'is_dark': dark}
+			debug = {'X': x, 'Y': y, 'R': @r, 'G': @g, 'B': @b, 'luminance value': l, 'is_dark': dark, 'inverted to white': inverted }
 			p debug
 		end
 		dark
